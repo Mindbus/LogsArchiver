0e166a9fd609b938fc1534a1e4365b1a30121f10
Joao Matos Silva <j.silva@mindbus.nl>
2017-02-08 16:30:34 +0000

    Added FileSystemInput and starting the main flow

--- src/LogsArchiver/Program.cs
+++ src/LogsArchiver/Program.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using LogsArchiver.Input;
+using Microsoft.Extensions.Configuration;
 
 namespace LogsArchiver
 {
     public class Program
     {
+        internal static IConfigurationRoot Configuration { get; private set; }
+
         public static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json");
+            Configuration = builder.Build();
+            
+            const string prefix = "LogsArchiver.Input.";
+            var inputTypeName = prefix + Configuration["input:type"];
+            var type = Type.GetType(inputTypeName);
+            var input = (IInput)Activator.CreateInstance(type, Configuration);
+
+            var files = input.GetFiles().Result;
+            foreach (var logFile in files)
+            {
+                Console.WriteLine($"{logFile.FileName} - {logFile.TimeStamp}");
+            }
         }
     }
 }
